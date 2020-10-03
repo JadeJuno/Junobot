8b9d552c9c011d849d312f5ed8d8075474c9f3d4
Golder06 <l.drago.supremo@gmail.com>
2020-10-03 12:51:20 -0300

    Finally added hypertext to the google command

--- Testing/Google Search.py
+++ Testing/Google Search.py
@@ -1,18 +1,7 @@
-from googlesearch import *
-import requests
-from bs4 import BeautifulSoup
-"""
-url = "http://www.amazon.in/s/ref=nb_sb_noss_1?url=search-alias%3Daps&field-keywords=python"
-source_code = requests.get(url)
-plain_text = source_code.content
-soup = BeautifulSoup(plain_text, "lxml")
-links = soup.findAll('a', {'class': 'a-link-normal s-access-detail-page a-text-normal'})
-prin(len(links))
-for link in links:
-	title = link.get('title')
-	print(title)
-"""
+import googlesearch
 
-browse = input(">")
-for html in search(browse, stop=10):
-	print(html)
+for result in googlesearch.search("a", stop=10):
+    print(result.title)
+    print(result.description)
+    print(result.link)
+    print()
