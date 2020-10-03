4c7154a7517414bcb3e793d4bb34206f2d893fdb
Golder06 <l.drago.supremo@gmail.com>
2020-10-03 13:33:22 -0300

    removed Testing folder (hopefully)

--- Testing/Google Search.py
+++
@@ -1,7 +0,0 @@
-import googlesearch
-
-for result in googlesearch.search("a", stop=10):
-    print(result.title)
-    print(result.description)
-    print(result.link)
-    print()
--- Testing/googletranslator.py
+++
@@ -1,7 +0,0 @@
-from googletrans import Translator
-
-translator = Translator()
-
-translate_this = input(">")
-print(translator.translate(translate_this).text)
-print(translator.detect(translate_this).lang)
