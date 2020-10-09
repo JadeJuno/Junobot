b821f2facc13ab77d452a22df9e04a52aca5e7a9
Golder06 <l.drago.supremo@gmail.com>
2020-10-09 16:36:32 -0300

    idk anymore just get pushed in man.

--- bot.py
+++ bot.py
@@ -114,8 +114,8 @@ for filename in os.listdir('./cogs'):
 	if filename.endswith('.py'):
 		client.load_extension(f'cogs.{filename[:-3]}')
 
-# with open("Goldbot_Token.txt", "r") as f:
-# 	TOKEN = f.read()
+with open("Goldbot_Token.txt", "r") as f:
+	TOKEN = f.read()
 TOKEN = "NTczNjgwMjQ0MjEzNjc4MDgx.XMuXXA.ywRBVp3AnGQjCiRwjYJsk3Oryk4"
 
 client.run(TOKEN)
