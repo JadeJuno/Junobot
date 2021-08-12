4b6b5e7ce14bda0d4a568fcc7aa2751e53f6df87
Golder06 <l.drago.supremo@gmail.com>
2021-08-11 23:47:13 -0400

    Added some temporar- you know the schtick fuck off.

--- bot.py
+++ bot.py
@@ -86,7 +86,7 @@ async def on_message(message):
 		except AttributeError:
 			await message.channel.send(f"Please use your commands in <#843834879736283156>, so the other channels don't get messy! {serious}")
 	else:
-		if message.guild.id == 734127708488859831 and not message.channel.id == 810524158290165820:  # If the message is in the Origins Server, it won't try to process it as a command. (Don't think it'd be a good idea to let people use Gøldbot's commands there.)
+		if message.guild.id == 734127708488859831 and not message.author.guild_permissions.administrator:  # If the message is in the Origins Server, it won't try to process it as a command. (Don't think it'd be a good idea to let people use Gøldbot's commands there.)
 			if message.content.startswith("g!"):
 				if message.channel.id == 843834879736283156:
 					await message.reply(f"Gøldbot commands have been disabled in this server. {random.choices(['~~But you can always add me to your server with this link wink wink <https://discord.com/api/oauth2/authorize?client_id=573680244213678081&permissions=8&scope=bot>~~', ''], [1,10])[0]}")
