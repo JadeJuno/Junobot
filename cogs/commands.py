7cea7f35c8e44ae56b63ce647aed789bb8c9f0ed
Golder06 <l.drago.supremo@gmail.com>
2021-08-31 20:46:39 -0400

    Temp changes Yada Yada

--- cogs/commands.py
+++ cogs/commands.py
@@ -403,6 +403,18 @@ class Commands(commands.Cog):
 	async def datapacks(self, ctx):
 		await ctx.send('If you want to find datapacks with custom origins, balance changes, and recipes for the Orb of Origin to use, check out <#749571272635187342>.\n\nTo install a datapack, navigate to your world folder (found in `.minecraft/saves`) and drop the datapack as a ZIP file into the `datapacks` folder. The same process can be used on a server (the world folder would be on the root though), and if you are running a server. the datapack does *not* need to be installed by each user.\n\nWhen creating a single-player world, there is also a "Data Packs" button in the world creation screen. If you click this, you are able to drag datapacks directly into the Minecraft window to add them. Don\'t forget to move them from "available" to "selected" though. This allows you to install datapacks *before+ a world is generated!\n\nIf you are looking for information on how to create datapacks yourself, type `!wiki` or `!tutorial` in <#843834879736283156>.')
 
+	@commands.check(bot.is_in_origin_server)
+	@commands.command(name="<#843834879736283156>", aliases=('commands',))
+	async def ch_commands(self, ctx):
+		if ctx.channel.id != 843834879736283156:
+			serious = self.client.get_emoji(821796259333537813)
+			try:
+				await ctx.reference.resolved.reply(f"Please use your commands in <#843834879736283156>, so the other channels don't get messy! {serious}")
+			except AttributeError:
+				await ctx.channel.send(f"Please use your commands in <#843834879736283156>, so the other channels don't get messy! {serious}")
+		else:
+			await ctx.reply("This message is already in <#843834879736283156>...")
+
 
 def setup(client):
 	client.add_cog(Commands(client))
