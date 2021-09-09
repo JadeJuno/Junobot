52cc19ae38ae350bfb16725865a82e1fc324d3a8
Golder06 <l.drago.supremo@gmail.com>
2021-09-09 03:30:08 -0300

    Temp changes yada yada

--- bot.py
+++ bot.py
@@ -193,15 +193,18 @@ async def on_message(message: discord.Message):
 				await autodelete(message)
 	else:
 		if is_in_origin_server(message) and not is_origin_mod(message):
-			if message.content.startswith("g!"):
-				if message.content.lstrip("g!").startswith(origin_commands):
+			if message.content.startswith(parser.__getitem__(message.channel.guild.id)):
+				if message.content.lstrip(parser.__getitem__(message.channel.guild.id)).startswith(origin_commands):
 					await client.process_commands(message)
 					return
-				if message.channel.id == 843834879736283156:
-					await message.reply(
-						f"This Goldbot commands has been disabled in this server. {random.choices(('~~But you can always add me to your server with this link wink wink <https://discord.com/api/oauth2/authorize?client_id=573680244213678081&permissions=8&scope=bot>~~', ''), (1, 10))[0]}")
-				else:
-					await message.reply("This Goldbot command has been disabled in this server.")
+				non_origin_commands = os.listdir("help_texts/specific_help")
+				non_origin_commands.remove("help")
+				if message.content.lstrip(parser.__getitem__(message.channel.guild.id)).startswith(non_origin_commands):
+					if message.channel.id == 843834879736283156:
+						await message.reply(
+							f"This Goldbot commands has been disabled in this server. {random.choices(('~~But you can always add me to your server with this link wink wink <https://discord.com/api/oauth2/authorize?client_id=573680244213678081&permissions=8&scope=bot>~~', ''), (1, 10))[0]}")
+					else:
+						await message.reply("This Goldbot command has been disabled in this server.")
 		else:
 			await client.process_commands(message)
 
