42f5d1a4dd09a38e5b4314150b5b2e7010642d55
Golder06 <l.drago.supremo@gmail.com>
2021-09-12 22:12:03 -0300

    Yes, yes I did...

--- cogs/commands.py
+++ cogs/commands.py
@@ -435,28 +435,17 @@ class Commands(commands.Cog):
 	@commands.command(aliases=('rules',))
 	async def rule(self, ctx, rule_index: int):
 		rules = (
-			"""**Rule 1: Be nice to each other.**
-			We want this to be a welcoming place for everyone. Keep in mind that not everyone has the same knowledge, background and experience as you.""",
-			"""**Rule 2: Keep it in English.**
-			We do not want to exclude others. But English is the language we all understand here, and the only language the moderator team can moderate. (This includes using the Latin alphabet as well, please don't use the Standard Galactic Alphabet or any other. :P)""",
-			"""**Rule 3: Appropriate content.**
-			This is a server about the Origins mod for Fabric. Please keep talk about that. If you want to talk about something else, move to off-topic. However, even in that channel we feel like certain controversial and inappropriate topics are out of place. A non-exhaustive list of topics which do not belong on this server includes: politics, religion, violence, and sexual content. In the end though, it's the call of the moderator that is present to decide what is appropriate and what isn't.""",
-			"""**Rule 4: Follow the Discord Terms of Service.**.
-			Discord does not allow talking about illegally distributed software, such as cracked Minecraft clients. Therefore we don't help with or want you to talk about cracked (non-premium) Minecraft clients and servers.""",
-			"""**Rule 5: Check <#740658667161911296> before asking.**
-			Many questions have already been answered in the <#740658667161911296> channel. Because of the large amount of new members, it is very time consuming to answer the same questions over and over and again. Therefore we would appreciate it if you could check that channel first and see if you can find a solution to your problem there.""",
-			"""**Rule 6: Don't spam the advertisement channels.**
-			Please only advertise your server in <#749228091708014635>, and your content in <#809807082895048764>, once every 7 days. Depending on the server activity, this frequency might change in the future.""",
-			"""**Rule 7: No transactions.**
-			Please do not offer or accept money or other forms of compensation for working on data packs or other content on this Discord. This is not the platform for this kind of transaction, and we won't take responsibility. Thus, we want you to keep that stuff out of this community.""",
-			"""**Rule 8: Keep stuff that happened in other servers in those servers (or DMs).**
-			We can't confirm whether what you're saying actually happened the way you say it did. We can only moderate what happens in this Discord. Therefore we'd like to ask you to not take arguments or public accusations to this server, if someone for example behaved badly on your own Discord or Minecraft server. Similarly, if someone is misbehaving to you in DMs, report them to the Discord staff. It is not our duty (nor are we able to) to sort out your personal arguments. However, we will reserve the right to remove a server from the advertisement channels if the content of those servers seems inappropriate to us.""",
-			"""**Rule 9: No Begging**
-			Datapacks, addons, etc. are not easy to make, I think that we can all agree on that. So please, do not beg or ask other people to make something for you or anyone else. You can always do your best to learn yourself and then you have the ability to make whatever you may like! All we ask is that you don't beg others to make anything for anyone. (However, giving ideas in channels such as <#798545973554315304> is not the same as this. Spamming your idea in this channel is still breaking this rule.)""",
-			"""**Rule 10: Keep your profile clean.**
-			While your profile picture and custom status are part of your online identity and we don't want to restrict that, they are inherently public and visible to all users on this server. Thus we will ask you to change your profile picture or status (or nickname) if they break the Discord ToS or Community Guidelines. If you didn't change it after 24 hours, we will kick you and report your profile to the Discord staff.""",
-			"""**Rule 11: Don't spam / Respect discussion.**
-			The point of this server is to help people with the Origins mod, talk about and discuss the base mod and datapacks, distribute datapacks so others can enjoy them, discuss the design of Origins, or build a friendly community in off-topic. Please be respectful with regards to other people's ongoing discussions. That does not mean that you are not allowed to talk to others while a discussion is going on - but purposefully interrupting them is not okay, and general spamming behavior is not tolerated."""
+			"**Rule 1: Be nice to each other.**\nWe want this to be a welcoming place for everyone. Keep in mind that not everyone has the same knowledge, background and experience as you.",
+			"**Rule 2: Keep it in English.**\nWe do not want to exclude others. But English is the language we all understand here, and the only language the moderator team can moderate. (This includes using the Latin alphabet as well, please don't use the Standard Galactic Alphabet or any other. :P)",
+			"**Rule 3: Appropriate content.**\nThis is a server about the Origins mod for Fabric. Please keep talk about that. If you want to talk about something else, move to off-topic. However, even in that channel we feel like certain controversial and inappropriate topics are out of place. A non-exhaustive list of topics which do not belong on this server includes: politics, religion, violence, and sexual content. In the end though, it's the call of the moderator that is present to decide what is appropriate and what isn't.",
+			"**Rule 4: Follow the Discord Terms of Service.**\nDiscord does not allow talking about illegally distributed software, such as cracked Minecraft clients. Therefore we don't help with or want you to talk about cracked (non-premium) Minecraft clients and servers.",
+			"**Rule 5: Check <#740658667161911296> before asking.**\nMany questions have already been answered in the <#740658667161911296> channel. Because of the large amount of new members, it is very time consuming to answer the same questions over and over and again. Therefore we would appreciate it if you could check that channel first and see if you can find a solution to your problem there.",
+			"**Rule 6: Don't spam the advertisement channels.**\nPlease only advertise your server in <#749228091708014635>, and your content in <#809807082895048764>, once every 7 days. Depending on the server activity, this frequency might change in the future.",
+			"**Rule 7: No transactions.**\nPlease do not offer or accept money or other forms of compensation for working on data packs or other content on this Discord. This is not the platform for this kind of transaction, and we won't take responsibility. Thus, we want you to keep that stuff out of this community.",
+			"**Rule 8: Keep stuff that happened in other servers in those servers (or DMs).**\nWe can't confirm whether what you're saying actually happened the way you say it did. We can only moderate what happens in this Discord. Therefore we'd like to ask you to not take arguments or public accusations to this server, if someone for example behaved badly on your own Discord or Minecraft server. Similarly, if someone is misbehaving to you in DMs, report them to the Discord staff. It is not our duty (nor are we able to) to sort out your personal arguments. However, we will reserve the right to remove a server from the advertisement channels if the content of those servers seems inappropriate to us.",
+			"**Rule 9: No Begging**\nDatapacks, addons, etc. are not easy to make, I think that we can all agree on that. So please, do not beg or ask other people to make something for you or anyone else. You can always do your best to learn yourself and then you have the ability to make whatever you may like! All we ask is that you don't beg others to make anything for anyone. (However, giving ideas in channels such as <#798545973554315304> is not the same as this. Spamming your idea in this channel is still breaking this rule.)",
+			"**Rule 10: Keep your profile clean.**\nWhile your profile picture and custom status are part of your online identity and we don't want to restrict that, they are inherently public and visible to all users on this server. Thus we will ask you to change your profile picture or status (or nickname) if they break the Discord ToS or Community Guidelines. If you didn't change it after 24 hours, we will kick you and report your profile to the Discord staff.",
+			"**Rule 11: Don't spam / Respect discussion.**\nThe point of this server is to help people with the Origins mod, talk about and discuss the base mod and datapacks, distribute datapacks so others can enjoy them, discuss the design of Origins, or build a friendly community in off-topic. Please be respectful with regards to other people's ongoing discussions. That does not mean that you are not allowed to talk to others while a discussion is going on - but purposefully interrupting them is not okay, and general spamming behavior is not tolerated."
 		)
 		try:
 			rule = rules[rule_index - 1]
@@ -473,18 +462,7 @@ class Commands(commands.Cog):
 	@commands.check(bot.is_in_origin_server)
 	@commands.command(aliases=("avd", "addonsvsdatapacks"))
 	async def addonvsdatapack(self, ctx):
-		output = """When discussing datapacks and addons, it is important, for the sake of specificity, to understand the difference:
-**Addons**
-Addons are actual minecraft mods written in Java inside a .jar file
-These add new features to the game and, in this case, the Origins Mod
-Additionally, these are put into the `.minecraft/mods` folder
-
-**Datapacks**
-Datapacks are content packs that use existing features within minecraft
-These are commonly written in JSON and using MCFunction files inside a zip folder
-Additionally, these are localized to a specific minecraft world in the `.minecraft/saves/{worldname}/datapacks` folder
-
-Most Origins are Datapacks and can be located in #datapacks. Otherwise the origin may be an addon and can be found on curseforge."""
+		output = """When discussing datapacks and addons, it is important, for the sake of specificity, to understand the difference:\n\n**Addons**\nAddons are actual minecraft mods written in Java inside a .jar file\nThese add new features to the game and, in this case, the Origins Mod\nAdditionally, these are put into the `.minecraft/mods` folder\n\n**Datapacks**\n\nDatapacks are content packs that use existing features within minecraft\nThese are commonly written in JSON and using MCFunction files inside a zip folder\nAdditionally, these are localized to a specific minecraft world in the `.minecraft/saves/{worldname}/datapacks` folder\n\nMost Origins are Datapacks and can be located in #datapacks. Otherwise the origin may be an addon and can be found on curseforge."""
 		await bot.tryreply(ctx, output)
 
 	@commands.check(bot.is_in_origin_server)
