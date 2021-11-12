68b1a6b590989d83d9cef1a7d7e4bd85aacb9234
Golder06 <l.drago.supremo@gmail.com>
2021-11-12 02:10:10 -0300

    Add a warning for using `?rdd` on datapack-dev

--- cogs/dyno.py
+++ cogs/dyno.py
@@ -42,6 +42,7 @@ class Dyno(commands.Cog):
 		try:
 			if ctx.channel.parent_id == 810587422303584286:
 				return await ctx.reply("This message is already in <#810587422303584286>...")
+			await bot.tryreply(ctx, 'https://media.discordapp.net/attachments/810587422303584286/867459174422020146/637612768361327151Datapack_Debugging_Chart_UPDATED.png')
 		except AttributeError:
 			await bot.tryreply(ctx, 'https://media.discordapp.net/attachments/810587422303584286/867459174422020146/637612768361327151Datapack_Debugging_Chart_UPDATED.png')
 
