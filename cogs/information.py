dc97e04baa0660e3aa4660ac95a9c593451023ca
Golder06 <53577918+Golder06@users.noreply.github.com>
2022-08-19 21:01:08 -0400

    declared temporary mantainance on the commands `g!translate` and `g!language`.

--- cogs/information.py
+++ cogs/information.py
@@ -96,6 +96,9 @@ class Information(commands.Cog):
 		}
 	)
 	async def lang_detect(self, ctx: commands.Context, *, sentence):
+		await botutils.error_template(ctx,
+									  "This command is currently under mantainance. Please be patient until we find a fix.")
+		return
 		detected_lang = self.get_text_language(sentence)
 		lang_name = languages.get(alpha2=detected_lang.lang[:2]).name
 		if detected_lang.confidence:
@@ -111,6 +114,9 @@ class Information(commands.Cog):
 		}
 	)
 	async def translate(self, ctx, translate_message, destination_language='en', source_language=None):
+		await botutils.error_template(ctx, "This command is currently under mantainance. Please be patient until we find a fix.")
+		return
+
 		destination_language = destination_language.lower()
 		destination_language = self.get_dict_key(self.lang_dict, destination_language)
 		if source_language is not None:
