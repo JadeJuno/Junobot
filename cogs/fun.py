216693d40c50c5685b920d83b3459ca3a38224c8
Golder06 <53577918+Golder06@users.noreply.github.com>
2022-11-26 17:58:53 -0300

    Move `get_mc_user` staticmethod to the end of the `Fun` cog.

--- cogs/fun.py
+++ cogs/fun.py
@@ -139,18 +139,6 @@ class Fun(commands.Cog):
 		await ctx.message.delete()
 		await channel.send(message, files=files)
 
-	@staticmethod
-	async def get_mc_user(name: str) -> tuple[str, str]:
-		name = urlparse.quote(name)
-
-		async with aiohttp.ClientSession() as session:
-			async with session.get(f"https://api.mojang.com/users/profiles/minecraft/{name}") as mc_request:
-				if mc_request.status == 200:
-					d = await mc_request.json()
-					return d['name'], d['id']
-				else:
-					raise ValueError()
-
 	@commands.command(
 		aliases=('skin',),
 		description="Sends the Minecraft skin of a user you give.",
@@ -171,6 +159,17 @@ class Fun(commands.Cog):
 		                                footer="Powered by craftafar.com")
 
 		await ctx.send(embed=embed)
+
+	@staticmethod
+	async def get_mc_user(name: str) -> tuple[str, str]:
+		name = urlparse.quote(name)
+		async with aiohttp.ClientSession() as session:
+			async with session.get(f"https://api.mojang.com/users/profiles/minecraft/{name}") as mc_request:
+				if mc_request.status == 200:
+					d = await mc_request.json()
+					return d['name'], d['id']
+				else:
+					raise ValueError
 
 
 async def setup(bot: commands.Bot):
